3b1d90f9c4123c152084bd530b22906dfa169ccb
子明 <51293485+ziming1@users.noreply.github.com>
2019-06-06 22:32:25 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,6 @@ import 'package:netease_music_api/netease_cloud_music.dart';
 
 void main() {
   debugDefaultTargetPlatformOverride = TargetPlatform.android;
-  startServer(address: '127.0.0.1', port: 3000);
   final settings = Settings();
   neteaseRepository = NeteaseRepository(settings);
   runApp(MyApp(setting: settings));
